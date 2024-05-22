5b2e80a4f2691bf7d4b86423ed66ad168481ac84
Ajayi12 <timothyajayi58@gmail.com>
2024-05-22 11:57:52 +0100

    python if/else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@
 for i in range(10):
     num2 = i + 1
     for n in range(num2, 10):
-        print("{}{}".format(i, n), end=", ")
-print()
+        if i == 8 and n == 9:
+            print("{}{}".format(i, n))  
+        else:
+            print("{}{}".format(i, n), end=", ")
