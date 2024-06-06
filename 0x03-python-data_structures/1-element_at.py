08d70b949d37ea29da16307cbb0e7816cc32b422
Ajayi12 <timothyajayi58@gmail.com>
2024-06-06 12:48:32 +0100

    python - list & tuple

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,7 +7,6 @@ def element_at(my_list, idx):
     length = len(my_list)
     if idx < 0:
         return None
-    elif idx > length:
+    if idx > length:
         return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
