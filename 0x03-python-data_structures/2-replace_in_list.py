f790af7ac970b1f5e400700e8def34c248258081
Ajayi12 <timothyajayi58@gmail.com>
2024-06-06 12:53:26 +0100

    python - list & tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,7 +7,7 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx < 0:
         return my_list
-    if idx > length:
+    if idx > length - 1:
         return my_list
     my_list[idx] = element
     return my_list
